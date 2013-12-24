a2bcd8d4068bad9282bd66eb333f128db162222e
Kale Franz <kale@franz.io>
2013-12-24 15:36:15 -0600

    site update

--- static/js/social-khVX1JQVmAJs.js
+++
@@ -1,3 +0,0 @@
-//http://stackoverflow.com/questions/19060289/social-media-buttons-slow-down-website-load-time
-/* Twitter */
-!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
